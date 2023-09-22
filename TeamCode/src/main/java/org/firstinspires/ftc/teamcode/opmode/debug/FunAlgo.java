b05ff7094bb102a3be5ca2c29c25388dee97cc5a
ProDCG <mason.stuart05@outlook.com>
2023-09-22 00:51:35 -0700

    👩‍🍳👩‍🍳👩‍🍳👩‍🍳👩‍🍳👩‍🍳👨‍🍳👨‍🍳👨‍🍳👨‍🍳👨‍🍳👨‍🍳👨‍🍳(●'◡'●)(●'◡'●)(●'◡'●)(●'◡'●)(^///^)(┬┬﹏┬┬)(┬┬﹏┬┬)(┬┬﹏┬┬)ᓚᘏᗢ(❁´◡`❁)(❁´◡`❁)ΦΦΦΦΦὢὢὮὠὨὨῳὣὥὥϛϲϺͿϲϲ⨊⨇⨉⨌⨌⨌⨌⨌⨌⨌⨌⨌⨌⨌⨞⨞⨞⨞⨋⨋⨑⨔⨔⨨⨟⨾⨾⩁⨿⨿⨿⩂⩂⩃⩀⨿⨿⩎⨼⨼⨼⨼⨼⩭⩭⩭⩮⩮⩨⩥⩤⩤⩤⩤⩥⩤⩤⩨⩨⩧⩥⩨⩨⩨

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/debug/FunAlgo.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.opmode.debug;
+
+import android.graphics.Canvas;
+
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+
+public class FunAlgo implements VisionProcessor {
+
+
+    private Mat cameraMatrix = new Mat(3, 3, CvType.CV_32F);
+
+    private final Mat intrinsics = new Mat(3, 3, CvType.CV_32F);
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        cameraMatrix.put(0, 0, 1, 1, 1);
+        cameraMatrix.put(1, 0, 1, 1, 1);
+        cameraMatrix.put(2, 2, 1);
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+
+        MatOfPoint2f pixelCords = new MatOfPoint2f(
+                new Point(1, 1), // 2D pixel coordinates of the first tag
+                new Point(1, 1),     // 2D pixel coordinates of the second tag
+                new Point(1, 1)      // 2D pixel coordinates of the third tag
+        );
+
+        MatOfPoint3f poseCords = new MatOfPoint3f(
+                new Point3(1, 1, 1), // 3D pose coordinates of the first tag
+                new Point3(1, 1, 1), // 3D pose coordinates of the second tag
+                new Point3(1, 1, 1) // 3D pose coordinates of the third tag
+        );
+
+        MatOfPoint3f arbitrary_3d_pose_value = new MatOfPoint3f(new Point3(1, 1, 1));
+
+        MatOfDouble distCoeffs = new MatOfDouble(1, 1, 1, 1, 1);
+
+        Mat rvec = new Mat();
+        Mat tvec = new Mat();
+
+        Calib3d.solvePnP(poseCords, pixelCords, cameraMatrix, distCoeffs, rvec, tvec);
+
+        MatOfPoint2f object2DLocation = new MatOfPoint2f();
+        Calib3d.projectPoints(arbitrary_3d_pose_value, rvec, tvec, cameraMatrix, distCoeffs, object2DLocation);
+
+        return object2DLocation;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/debug/FunAlgoTest.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode.opmode.debug;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+public class FunAlgoTest extends LinearOpMode {
+
+    private static final boolean USE_WEBCAM = true;
+
+    private AprilTagProcessor aprilTag;
+
+    private VisionPortal visionPortal;
+
+    private void initAprilTag() {
+
+        aprilTag = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagOutline(true)
+                .setDrawTagID(true)
+//                .setTagFamily()
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.RADIANS)
+
+//                .setLensIntrinsics(fx, fy, cx, cy)
+
+                .build();
+
+//        WebcamName cam = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+//        FtcDashboard.getInstance().startCameraStream(, 60);
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+        builder.enableLiveView(true);
+
+        builder.setCameraResolution(new Size(480, 360));
+        // builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
+
+        builder.addProcessor(aprilTag);
+
+        visionPortal = builder.build();
+
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initAprilTag();
+
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+
+                telemetryAprilTag();
+
+                telemetry.update();
+
+                if (gamepad1.dpad_down) {
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    visionPortal.resumeStreaming();
+                }
+
+                sleep(20);
+            }
+        }
+
+        visionPortal.close();
+    }
+
+    private void telemetryAprilTag() {
+
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
+
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+
+    }
+}

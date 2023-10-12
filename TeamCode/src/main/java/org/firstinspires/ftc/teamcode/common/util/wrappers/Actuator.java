275b00fa17f0385597d3e2b1cba918fa5dff5f43
ProDCG <mason.stuart05@outlook.com>
2023-10-12 06:59:38 -0700

    i was bored and added javadoc comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/wrappers/Actuator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/wrappers/Actuator.java
@@ -31,20 +31,22 @@ public class Actuator {
     private double tolerance = 0.0;
     private boolean reached = false;
 
-    public Actuator(PIDController controller, HardwareDevice... devices) {
-        this.controller = controller;
-
-        for (HardwareDevice device : devices) {
-            this.devices.put(device.getDeviceName(), device);
-        }
-    }
-
+    /**
+     * Actuator constructor with varargs HardwareDevice parameter
+     *
+     * @param devices
+     */
     public Actuator(HardwareDevice... devices) {
         for (HardwareDevice device : devices) {
             this.devices.put(device.getDeviceName(), device);
         }
     }
 
+    /**
+     * Reads every given hardware device containing either AnalogEncoder or Encoder object.
+     * Will then store this, and terminate the loop, because there is only a need for one value in
+     * a given actuation group.
+     */
     public void read() {
         for (HardwareDevice device : devices.values()) {
             if (device instanceof AbsoluteAnalogEncoder) {
@@ -57,6 +59,11 @@ public class Actuator {
         }
     }
 
+    /**
+     * Performs arithmetic with the AsymmetricMotionProfile and PIDController.
+     * Stores a boolean representing whether or not the actuator group is within
+     * some tolerance given by a specified value.
+     */
     public void periodic() {
         if (profile != null) {
             this.state = profile.calculate(timer.time());
@@ -70,6 +77,10 @@ public class Actuator {
         this.reached = Math.abs(targetPosition - position) < tolerance;
     }
 
+    /**
+     * For cohesiveness in our program's sequence, writes back all of the new
+     * values calculated and saved. Runs different methods based on the given actuation group.
+     */
     public void write() {
         for (HardwareDevice device : devices.values()) {
             if (device instanceof DcMotor) {
@@ -80,41 +91,91 @@ public class Actuator {
         }
     }
 
+    /**
+     * Will set the target position for the actuator group. In the case of a motion profile
+     * being used, the profile will be reset and created again with the new target position.
+     * Otherwise, a PIDController will be used in the periodic() method above.
+     *
+     * @param targetPosition
+     */
     public void setTargetPosition(double targetPosition) {
         if (this.profile != null) {
-            this.setMotionProfile(new AsymmetricMotionProfile(position, targetPosition, constraints));
+            this.setMotionProfile(new AsymmetricMotionProfile(position, targetPosition, this.profile.constraints));
             this.timer.reset();
         } else {
             this.targetPosition = targetPosition;
         }
     }
 
+    /**
+     * Gets the value read by the actuation group.
+     *
+     * @return double
+     */
     public double getPosition() {
         return position;
     }
 
+    /**
+     * Gets any given HardwareDevice within this actuator group.
+     *
+     * @param deviceName The given HardwareMap name specified in the config,
+     *                   or specified at object creation.
+     * @return HardwareDevice object
+     */
     public HardwareDevice getDevice(String deviceName) {
         return this.devices.get(deviceName);
     }
 
+    /**
+     * Returns a list of all given HardwareDevices.
+     *
+     * @return
+     */
     public List<HardwareDevice> getDevices() {
         return new ArrayList<>(devices.values());
     }
 
+    /**
+     * Returns whether or not the given actuation group is within error
+     * tolerance of the final position.
+     *
+     * @return
+     */
     public boolean hasReached() {
         return this.reached;
     }
 
+    /**
+     * Saves the value passed in for the new motion profile.
+     *
+     * @param profile The new asymmetrical motion profile
+     * @return
+     */
     public Actuator setMotionProfile(AsymmetricMotionProfile profile) {
         this.profile = profile;
         return this;
     }
 
+    /**
+     * Saves the value passed in for the new PID controller.
+     *
+     * @param controller
+     * @return
+     */
     public Actuator setPIDController(PIDController controller) {
         this.controller = controller;
         return this;
     }
 
+    /**
+     * Creates a new PIDController object based on the coefficients passed in.
+     *
+     * @param p Proportional constant
+     * @param i Integral Constant
+     * @param d Derivative Constant
+     * @return
+     */
     public Actuator setPID(double p, double i, double d) {
         if (controller == null) {
             this.controller = new PIDController(p, i, d);
@@ -125,6 +186,13 @@ public class Actuator {
         return this;
     }
 
+    /**
+     * Sets the allowed error tolerance for the actuation group to be considered
+     * "close enough" within the target position.
+     *
+     * @param tolerance
+     * @return
+     */
     public Actuator setErrorTolerance(double tolerance) {
         this.tolerance = tolerance;
         return this;
